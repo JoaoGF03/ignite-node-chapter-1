b9094c41019670bbdbef33e69c141ff7f4966765
Guilherme <joao.g.f@icloud.com>
2022-06-17 18:20:13 -0300

    challenges completed

---
+++ README.md
@@ -0,0 +1,8 @@
+# Chapter I ignite-node-challenges
+
+### Instruções
+[Desafio-01: Conceitos do Node.js](https://www.notion.so/Desafio-01-Conceitos-do-Node-js-59ccb235aecd43a6a06bf09a24e7ede8)
+
+[Desafio-02: Trabalhando com Middlewares](https://www.notion.so/Desafio-02-Trabalhando-com-middlewares-4f89bf538c2e4ee291382b92bdc36790)
+
+[Desafio-03: Corrigindo o código](https://www.notion.so/Desafio-03-Corrigindo-o-c-digo-c15c8a2e212846039a367cc7b763c6dd)
---
+++ desafio-01/src/index.js
@@ -0,0 +1,131 @@
+const express = require('express');
+const cors = require('cors');
+
+const { v4: uuid } = require('uuid');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const users = [];
+
+function checksExistsUserAccount(request, response, next) {
+  const { username } = request.headers;
+
+  const user = users.find(user => user.username === username);
+
+  if (!user) {
+    return response.status(404).json({ error: 'User does not exists.' });
+  }
+
+  return next();
+}
+
+app.post('/users', (request, response) => {
+  const { name, username } = request.body;
+
+  const userAlreadyExists = users.find(user => user.username === username);
+
+  if (userAlreadyExists) {
+    return response.status(400).json({ error: 'User already exists.' });
+  }
+
+  const user = {
+    id: uuid(),
+    name,
+    username,
+    todos: [],
+  };
+
+  users.push(user);
+
+  return response.status(201).json(user);
+});
+
+app.get('/todos', checksExistsUserAccount, (request, response) => {
+  const { username } = request.headers;
+
+  const user = users.find(user => user.username === username);
+
+  return response.json(user.todos);
+});
+
+app.post('/todos', checksExistsUserAccount, (request, response) => {
+  const { username } = request.headers;
+
+  const { title, deadline } = request.body;
+
+  const user = users.find(user => user.username === username);
+
+  const todo = {
+    id: uuid(),
+    title,
+    done: false,
+    deadline: new Date(deadline),
+    created_at: new Date(Date.now()),
+  };
+
+  user.todos.push(todo);
+
+  return response.status(201).json(todo);
+});
+
+app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
+  const { username } = request.headers;
+
+  const { id } = request.params;
+
+  const { title, deadline } = request.body;
+
+  const user = users.find(user => user.username === username);
+
+  const todo = user.todos.find(todo => todo.id === id);
+
+  if (!todo) {
+    return response.status(404).json({ error: 'Todo does not exists.' });
+  }
+
+  todo.title = title;
+  todo.deadline = new Date(deadline);
+
+  return response.json(todo);
+});
+
+app.patch('/todos/:id/done', checksExistsUserAccount, (request, response) => {
+  const { username } = request.headers;
+
+  const { id } = request.params;
+
+  const user = users.find(user => user.username === username);
+
+  const todo = user.todos.find(todo => todo.id === id);
+
+  if (!todo) {
+    return response.status(404).json({ error: 'Todo does not exists.' });
+  }
+
+  todo.done = !todo.done;
+
+  return response.json(todo);
+});
+
+app.delete('/todos/:id', checksExistsUserAccount, (request, response) => {
+  const { username } = request.headers;
+
+  const { id } = request.params;
+
+  const user = users.find(user => user.username === username);
+
+  const todo = user.todos.find(todo => todo.id === id);
+
+  if (!todo) {
+    return response.status(404).json({ error: 'Todo does not exists.' });
+  }
+
+  user.todos = user.todos.filter(todo => todo.id !== id);
+
+  return response.status(204).json()
+});
+
+module.exports = app;
